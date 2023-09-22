92c655a5474f9a69049fe47e71c836d9af4ca67f
cisalpinsiso <145431599+cisalpinsiso@users.noreply.github.com>
2023-09-22 16:32:14 +0200

    Backend

---
+++ back/main.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+const app = express();
+app.use(express.json());
+app.use(session({
+    secret: 'dsof82445qs*2E',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'm2l',
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+});
+
+app.get('/api', (req, res) => {
+    res.send('API is up');
+});
+
+app.get('/api/user', (req, res) => {
+    if (req.session.user) {
+        res.send(req.session.user);
+    } else {
+        res.send({ 'message': 'error' });
+    }
+});
+
+const articles = [
+    {
+        'id': '123',
+        'nom': 'article 1',
+        'quantite': '10',
+        'prix': '100',
+        'description': 'description 1'
+    }
+]
+
+app.get('/api/articles', (req, res) => {
+    res.send(articles);
+});
+
+app.post('/api/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    pool.query('SELECT * FROM utilisateurs WHERE email = ?', [email], (err, rows) => {
+        if (err) {
+            res.send({ 'error': err });
+        } else {
+            if (rows.length > 0) {
+                bcrypt.compare(password, rows[0].mdp, (err, result) => {
+                    if (result) {
+                        req.session.user = rows;
+                        res.send({ 'message': 'success' });
+                    } else {
+                        res.send({ 'message': 'error' });
+                    }
+                });
+            } else {
+                res.send({ 'message': 'error' });
+            }
+        }
+    });
+});
+
+app.get('/api/logout', (req, res) => {
+    req.session.destroy();
+    res.send({ 'message': 'success' });
+});
+
+app.post('/api/register', (req, res) => {
+    const nom = req.body.nom;
+    const prenom = req.body.prenom;
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirm = req.body.confirm;
+
+    if (password === confirm) {
+        bcrypt.hash(password, 10, (err, hash) => {
+            if (err) {
+                res.send({ 'error': err });
+            } else {
+                pool.query('INSERT INTO utilisateurs (nom, prenom, email, mdp) VALUES (?, ?, ?, ?)', [nom, prenom, email, hash], (err, rows) => {
+                    if (err) {
+                        res.send({ 'error': err });
+                    } else {
+                        res.send({ 'message': 'success' });
+                    }
+                });
+            }
+        });
+    } else {
+        res.send({ 'message': 'error' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
---
+++ back/readme.md
@@ -0,0 +1,12 @@
+# Installation
+⚠️ La base de donnée sous MySQL doit être lancée
+1. `npm install`
+2. `node main.js`
+# Endpoints
+| Endpoint | Method | Description           | Note |
+|----------|--------|-----------------------|----- 
+| /api     | GET    | Vérifier si l'api est en ligne         | 
+| /api/user    | GET    | Obtenir des informations sur la session de l'utilisateur     | Utiliser dans un useEffect pour obtenir le pseudo et autre
+| /api/articles | GET | Obtenir la liste des articles | Utiliser dans un useEffect dans la page produits
+| /api/login | POST | Se connecter | Fournir il` et `password`
+| /api/register | POST | Créer un compte | Fournir `prenom` `nom` `email` `password` `confirm`
