4a04279cb83cb80674a8f87171072a416c17d637
Hassan.B <hassan.boucherit@gmail.com>
2023-10-10 17:01:16 +0200

    push navbar

--- front/src/layout/Navbar.jsx
+++ front/src/layout/Navbar.jsx
@@ -25,36 +25,24 @@ function Navbar() {
         <div className="topnav">
           <img src="src/assets/Logo2.png" className="" alt="" />
 
-          <button className="btn btn-primary" type="submit">
-            Button
+          <button type="button" class="btn btn-secondary">
+            <i class="bi bi-house"></i>
+            Accueil
           </button>
-          <button variant="outlined">Boutique</button>
+          <button type="button" class="btn btn-secondary">
+            <i class="bi bi-cart"></i>
+            Boutique
+          </button>
         </div>
 
         <div className="bottomnav">
-          <button onClick={handleOpen}>{user}</button>
-          <div className="modal"
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <div sx={style}>
-              <h6 id="modal-modal-title" variant="h6" component="h2">
-                {user}
-              </h6>
-              <p id="modal-modal-description" sx={{ mt: 2 }}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-                rem reiciendis fuga sed ut tempore minus, dignissimos,
-                praesentium voluptatibus natus magni cumque laborum nesciunt
-                fugit nam. Veniam voluptates ut qui?
-              </p>
-            </div>
-          </div>
-
-          <button variant="contained">
-          <i className="bi bi-0-square"></i>
- Deconnexion
+          <button type="button" class="btn btn-secondary">
+            <i class="bi bi-person-fill"></i>
+            Mon compte
+          </button>
+          <button type="button" class="btn btn-warning">
+            <i class="bi bi-box-arrow-right" color="white"></i>
+            Deconnexion
           </button>
         </div>
       </nav>
