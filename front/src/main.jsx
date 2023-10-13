163316c28dd272fa192a0f94c920cd1c5c116cfa
Yaki00 <116871307+Yaki00@users.noreply.github.com>
2023-10-13 17:04:56 +0200

    maj css

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import './index.css'
+import './App.css'
+import './layout/Navbar.css'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
