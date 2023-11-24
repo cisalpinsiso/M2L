75c7822c47d17cae36a156a24ecb6ae99d4d972b
cisalpinsiso <145431599+cisalpinsiso@users.noreply.github.com>
2023-11-24 16:46:24 +0100

    Réparation

--- front/src/contenu/accueil/index.jsx
+++ front/src/contenu/accueil/index.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import "./accueil.css";
-import  Articles  from "./articles/articles.jsx";
+
 import Carrousel from "./carousel";
 
 function Accueil(props) {
