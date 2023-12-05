d6392fa30a4cc97ebff3b6a6b3efc731da3d3d0d
Yaki00 <116871307+Yaki00@users.noreply.github.com>
2023-12-05 10:25:52 +0100

    05/12

--- front/src/contenu/accueil/index.jsx
+++ front/src/contenu/accueil/index.jsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from "react";
 import "./accueil.css";
 
 import Carrousel from "./carousel";
+import Article from "./article/article";
 
 function Accueil(props) {
 
   return (
     <>
     <Carrousel />
+    <Article />
     </>
   );
 }
