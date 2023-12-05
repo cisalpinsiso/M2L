6602b929472e92842394aaa2ac69db805785eace
Yaki00 <116871307+Yaki00@users.noreply.github.com>
2023-12-05 11:00:10 +0100

    recup

--- front/src/contenu/accueil/index.jsx
+++ front/src/contenu/accueil/index.jsx
@@ -7,21 +7,20 @@ import Article from "./article/article";
 function Accueil(props) {
   return (
     <>
-<<<<<<< HEAD
-    <Carrousel />
-    <Article />
-=======
-      <div className="firstblock">
-        <h3>
-          <span className="titre">Bienvenue sur notre site</span>
-        </h3>
-        <p>
-          <span className="titre">Nous sommes heureux de vous accueillir</span>
-        </p>
-       
-      </div>
       <Carrousel />
->>>>>>> f2819bed3d74039f4084bfc877f7cba58843d2fe
+      <Article />
+
+        <div className="firstblock">
+          <h3>
+            <span className="titre">Bienvenue sur notre site</span>
+          </h3>
+          <p>
+            <span className="titre">Nous sommes heureux de vous accueillir</span>
+          </p>
+        
+        </div>
+        <Carrousel />
+
     </>
   );
 }
