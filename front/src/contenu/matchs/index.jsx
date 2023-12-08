ecb94ef6206f5c4cd3193a8c3d4a30c0030fc688
cisalpinsiso <145431599+cisalpinsiso@users.noreply.github.com>
2023-12-08 12:13:05 +0100

    Push matchs

--- front/src/contenu/matchs/index.jsx
+++ front/src/contenu/matchs/index.jsx
@@ -1,31 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './matchs.css';
 
 function Matchs(props) {
-  const equipes = props.equipes;
   
   const [matches, setMatches] = useState([]);
+  const { equipes } = props;
+
+  const generateMatchs = () => {
+    let matchs = [];
+    for (let i = 0; i < 10; i++) {
+      let equipe1 = equipes[Math.floor(Math.random() * equipes.length)];
+      let equipe2 = equipes[Math.floor(Math.random() * equipes.length)];
+      let date = new Date();
+      date.setDate(date.getDate() + Math.floor(Math.random() * 10));
+      date.setHours(date.getHours() + Math.floor(Math.random() * 10));
+      date.setMinutes(date.getMinutes() + Math.floor(Math.random() * 60));
+      matchs.push({
+        equipe1: equipe1,
+        equipe2: equipe2,
+        date: date
+      });
+    }
+    setMatches(matchs);
+  }
 
   useEffect(() => {
-    if (equipes.length == 0) return;
-
-    for (let i = 0; i < 3; i++) {
-      let randomTeam1 = Math.floor(Math.random() * equipes.length)
-      let randomTeam2 = null;
-
-      while (randomTeam2 == null || randomTeam2 == randomTeam1) {
-        randomTeam2 = Math.floor(Math.random() * equipes.length)
-      }
-
-      let randomDate = new Date(2023, Math.floor(Math.random() * 12), Math.floor(Math.random() * 30), Math.floor(Math.random() * 24), Math.floor(Math.random() * 60), Math.floor(Math.random() * 60), Math.floor(Math.random() * 1000))
-
-      setMatches([...matches, {
-        equipe1: equipes[randomTeam1],
-        equipe2: equipes[randomTeam2],
-        date: randomDate
-      }])
-    }
-  }, [props.equipes])
+    if (equipes.length > 0)
+      generateMatchs();
+  }, [equipes]);
 
   return (
     <div className="matchs">
@@ -42,20 +44,20 @@ function Matchs(props) {
         </thead>
         <tbody>
           {
-            matches.map((match) => {
+            matches && matches.map((match) => {
               return (
                 <tr>
                   <td>
                     <img src={"/equipes" + match.equipe1.logo} alt="" />
                   </td>
                   <td>
-                    <p>{match.equipe1.name}</p>
+                    <p>{match.equipe1.nom}</p>
                   </td>
                   <td>
                     <img src={"/equipes" + match.equipe2.logo} alt="" />
                   </td>
                   <td>
-                    <p>{match.equipe2.name}</p>
+                    <p>{match.equipe2.nom}</p>
                   </td>
                   <td>
                     <p>{match.date.toLocaleString()}</p>
