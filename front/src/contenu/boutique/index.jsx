e7f069042ec3dc1edaf0e2afeac9e816d33cfcee
Hassan.B <hassan.boucherit@gmail.com>
2023-12-08 16:58:32 +0100

    accounts_push

--- front/src/contenu/boutique/index.jsx
+++ front/src/contenu/boutique/index.jsx
@@ -22,10 +22,10 @@ function Boutique(props) {
   }, []);
 
   const ajoutPanier = (productToAdd) => {
-    const existingProduct = props.panier.find(p => p.id === productToAdd.id);
+    const existingProduct = props.panier.find((p) => p.id === productToAdd.id);
 
     if (existingProduct) {
-      const newPanier = props.panier.map(p =>
+      const newPanier = props.panier.map((p) =>
         p.id === productToAdd.id ? { ...p, quantity: p.quantity + 1 } : p
       );
       props.setPanier(newPanier);
@@ -35,32 +35,48 @@ function Boutique(props) {
   };
 
   const retirerPanier = (productToRemove) => {
-    const existingProduct = props.panier.find(p => p.id === productToRemove.id);
+    const existingProduct = props.panier.find(
+      (p) => p.id === productToRemove.id
+    );
 
     if (existingProduct) {
       if (existingProduct.quantity > 1) {
-        const newPanier = props.panier.map(p =>
+        const newPanier = props.panier.map((p) =>
           p.id === productToRemove.id ? { ...p, quantity: p.quantity - 1 } : p
         );
         props.setPanier(newPanier);
       } else {
-        const newPanier = props.panier.filter(p => p.id !== productToRemove.id);
+        const newPanier = props.panier.filter(
+          (p) => p.id !== productToRemove.id
+        );
         props.setPanier(newPanier);
       }
     }
   };
 
   const commander = () => {
-    api.newCommande(JSON.stringify(props.panier.map(p => ({ id: p.id, quantity: p.quantity })))).then((response) => {
-      if (response.data) {
-        props.setPanier([]);
-      }
-    });
+    api
+      .newCommande(
+        JSON.stringify(
+          props.panier.map((p) => ({ id: p.id, quantity: p.quantity }))
+        )
+      )
+      .then((response) => {
+        if (response.data) {
+          props.setPanier([]);
+        }
+      });
   };
 
-  const sizePanier = props.panier.reduce((acc, product) => acc + product.quantity, 0);
+  const sizePanier = props.panier.reduce(
+    (acc, product) => acc + product.quantity,
+    0
+  );
 
-  const prixTotal = props.panier.reduce((acc, product) => acc + product.prix * product.quantity, 0);
+  const prixTotal = props.panier.reduce(
+    (acc, product) => acc + product.prix * product.quantity,
+    0
+  );
 
   return (
     <>
@@ -68,48 +84,100 @@ function Boutique(props) {
         <h3>
           Bonjour, <b>{props.user ? props.user.nom : "utilisateur"}</b>
         </h3>
-        <i className="bi bi-cart-fill cart position-relative" onClick={() => setShowCart(!showCart)}>
-          {sizePanier > 0 && <span className="badge bg-primary rounded-pill position-absolute">{sizePanier}</span>}
+        <i
+          className="bi bi-cart-fill cart position-relative"
+          onClick={() => setShowCart(!showCart)}
+        >
+          {sizePanier > 0 && (
+            <span className="badge bg-primary rounded-pill position-absolute">
+              {sizePanier}
+            </span>
+          )}
         </i>
       </div>
-      <div className={`offcanvas-backdrop fade ${showCart ? "show" : "pe-none"}`}></div>
-      <div className={`offcanvas offcanvas-end ${showCart ? "show" : "hiding"} z-index-1`} tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+      <div
+        className={`offcanvas-backdrop fade ${showCart ? "show" : "pe-none"}`}
+      ></div>
+      <div
+        className={`offcanvas offcanvas-end ${
+          showCart ? "show" : "hiding"
+        } z-index-1`}
+        tabindex="-1"
+        id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel"
+      >
         <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasExampleLabel">Panier</h5>
-          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={() => setShowCart(false)}></button>
+          <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+            Panier
+          </h5>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+            onClick={() => setShowCart(false)}
+          ></button>
         </div>
         <div className="offcanvas-body">
-          {props.panier.length === 0 ?
+          {props.panier.length === 0 ? (
             <div className="text-center">Votre panier est vide</div>
-            :
+          ) : (
             <div>
               <ul className="list-group position-relative">
                 {props.panier.map((product) => (
                   <li className="list-group-item d-flex justify-content-between align-items-center">
-                    <img src={product.image} alt={product.nom} style={{ width: "24px", marginRight: "10px" }} />
+                    <img
+                      src={product.image}
+                      alt={product.nom}
+                      style={{ width: "24px", marginRight: "10px" }}
+                    />
                     {product.nom}
-                    <div className="btn-group" role="group" aria-label="Basic example">
-                      <button type="button" className="btn" onClick={() => retirerPanier(product)}>
+                    <div
+                      className="btn-group"
+                      role="group"
+                      aria-label="Basic example"
+                    >
+                      <button
+                        type="button"
+                        className="btn"
+                        onClick={() => retirerPanier(product)}
+                      >
                         <i className="bi bi-dash-circle-fill"></i>
                       </button>
-                      <button type="button" className="btn" onClick={() => ajoutPanier(product)}>
+                      <button
+                        type="button"
+                        className="btn"
+                        onClick={() => ajoutPanier(product)}
+                      >
                         <i className="bi bi-plus-circle-fill"></i>
                       </button>
                     </div>
-                    <span className="badge bg-primary rounded-pill">{product.quantity}</span>
+                    <span className="badge bg-primary rounded-pill">
+                      {product.quantity}
+                    </span>
                   </li>
                 ))}
               </ul>
-              {props.user ?
+              {props.user ? (
                 <div>
-                  <div className="text-center mt-3">Prix total : {prixTotal} €</div>
-                  <button type="button" className="btn btn-primary mt-3 w-100" onClick={() => commander()}>Commander</button>
+                  <div className="text-center mt-3">
+                    Prix total : {prixTotal} €
+                  </div>
+                  <button
+                    type="button"
+                    className="btn btn-primary mt-3 w-100"
+                    onClick={() => commander()}
+                  >
+                    Commander
+                  </button>
                 </div>
-              :
-                <div className="text-center mt-3">Veuillez vous connecter pour commander</div>
-              }
+              ) : (
+                <div className="text-center mt-3">
+                  Veuillez vous connecter pour commander
+                </div>
+              )}
             </div>
-          }
+          )}
         </div>
       </div>
       <div ref={searchRef}>
@@ -130,9 +198,11 @@ function Boutique(props) {
             <select
               className="custom-select mr-sm-2 form-control"
               value={filterSelection}
-              onChange={(e) => setFilterSelection(e.target.value)}              
+              onChange={(e) => setFilterSelection(e.target.value)}
             >
-              <option value="all" selected>Choose...</option>
+              <option value="all" selected>
+                Choose...
+              </option>
               <option value="maillot">Maillots</option>
               <option value="équipement">Équipements</option>
               <option value="foot">Football</option>
@@ -141,17 +211,39 @@ function Boutique(props) {
         </form>
       </div>
       <div className="d-grid gap-3 produits">
-        {products.filter((product) => {
-          if (filterSelection === "all") {
-            return true;
-          } else {
-            return product.description.toLowerCase().includes(filterSelection.toLowerCase()) || product.nom.toLowerCase().includes(filterSelection.toLowerCase());
-          }
-        }).filter((product) => {
-          return product.nom.toLowerCase().includes(filterText.toLowerCase()) || product.description.toLowerCase().includes(filterText.toLowerCase());
-        }).map((product) => (
-          <Produit nom={product.nom} description={product.description} prix={product.prix} image={product.image} id={product.id} ajoutPanier={() => ajoutPanier(product)} />
-        ))}
+        {products
+          .filter((product) => {
+            if (filterSelection === "all") {
+              return true;
+            } else {
+              return (
+                product.description
+                  .toLowerCase()
+                  .includes(filterSelection.toLowerCase()) ||
+                product.nom
+                  .toLowerCase()
+                  .includes(filterSelection.toLowerCase())
+              );
+            }
+          })
+          .filter((product) => {
+            return (
+              product.nom.toLowerCase().includes(filterText.toLowerCase()) ||
+              product.description
+                .toLowerCase()
+                .includes(filterText.toLowerCase())
+            );
+          })
+          .map((product) => (
+            <Produit
+              nom={product.nom}
+              description={product.description}
+              prix={product.prix}
+              image={product.image}
+              id={product.id}
+              ajoutPanier={() => ajoutPanier(product)}
+            />
+          ))}
       </div>
     </>
   );
--- front/src/contenu/compte/index.jsx
+++ front/src/contenu/compte/index.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import CommandePage from "./commande/commande";
 
 function Compte(props) {
   const [email, setEmail] = useState("");
@@ -135,13 +136,7 @@ function Compte(props) {
             Commandes
           </Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <CommandePage />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item className="Accordion.Item" eventKey="5">
@@ -168,15 +163,7 @@ function Compte(props) {
             <i className="bi bi-android"></i>
             Application
           </Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
+          <Accordion.Body></Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
           <Accordion.Header>
